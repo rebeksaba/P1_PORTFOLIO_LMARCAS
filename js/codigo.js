0e8327ec1b546542415ad5f4d1befc4f736b724e
rebeksaba <rebeksaba@gmail.com>
2025-08-02 12:13:47 +0200

    resolución problemas de ruta java script y fuentes

--- js/codigo.js
+++ js/codigo.js
@@ -1,43 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
+  console.log("JavaScript cargado correctamente");
   // Menú responsive
   const menuBtn = document.querySelector(".menu-responsive");
   const menu = document.getElementById("menu");
-  const icono = menuBtn.querySelector("i");
+  const icono = menuBtn?.querySelector("i");
 
-  menuBtn.addEventListener("click", () => {
-    menu.classList.toggle("mostrar");
+  menuBtn?.addEventListener("click", () => {
+    menu?.classList.toggle("mostrar");
 
-    if (menu.classList.contains("mostrar")) {
-      icono.classList.remove("fa-bars");
-      icono.classList.add("fa-xmark");
+    if (menu?.classList.contains("mostrar")) {
+      icono?.classList.remove("fa-bars");
+      icono?.classList.add("fa-xmark");
     } else {
-      icono.classList.remove("fa-xmark");
-      icono.classList.add("fa-bars");
+      icono?.classList.remove("fa-xmark");
+      icono?.classList.add("fa-bars");
     }
   });
 
   // Popup de cookies
   const botonCookies = document.getElementById("aceptar-cookie");
   const popupCookies = document.getElementById("cookie-popup");
-  const seccionPrivacidad = document.getElementById("privacidad"); 
 
   if (localStorage.getItem("cookiesAceptadas")) {
-    popupCookies.classList.add("oculto");
-
-    
-    if (seccionPrivacidad) {
-      seccionPrivacidad.classList.add("oculto");
-    }
+    popupCookies?.classList.add("oculto");
   }
 
-  botonCookies.addEventListener("click", function () {
+  botonCookies?.addEventListener("click", function () {
     localStorage.setItem("cookiesAceptadas", true);
-    popupCookies.classList.add("oculto");
-
-    
-    if (seccionPrivacidad) {
-      seccionPrivacidad.classList.add("oculto");
-    }
+    popupCookies?.classList.add("oculto");
   });
 });
 
